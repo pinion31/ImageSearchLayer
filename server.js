c69a653d45eb2ec30a71dd102fc44cc6010947a1
Chris Cantu <pinion31@gmail.com>
2017-07-02 01:37:11 -0500

    code alpha

---
+++ server.js
@@ -0,0 +1,71 @@
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+var GoogleSearch = require('google-search');
+var googleSearch = new GoogleSearch({
+  key: 'AIzaSyB5lv4HA07uUcrtU3-oxKQl05oNk90LyIw',
+  cx: '009489450447108043722:hvm4sscmlww'
+});
+var mongodb = require('mongodb');
+var mongoClient = mongodb.MongoClient;
+var urlDB = process.env.MONGOLAB_URI;
+
+
+
+http.createServer(function(req,res) {
+  var resultArr = [];
+  var q = url.parse(req.url, true);
+  //console.log(q.query.search);
+ mongoClient.connect(urlDB, function(err, db) {
+   if (err) {return "Database Connection Error"};
+
+    if (q.query.history || q.query.search === "") {
+
+     db.collection('searchHistory').find({},{_id: 0}).sort({ $natural: -1 }).toArray(function(err, history) {
+        if (err) {return err};
+        res.end(JSON.stringify(history));
+        //res.end("return");
+     });
+
+    }
+    else if (q.query.search !== undefined) {
+      googleSearch.build({
+        q: q.query.search,
+        num: 10, // Number of search results to return between 1 and 10, inclusive
+        start: +q.query.offset > 0? +q.query.offset: 1,
+
+      }, function(error, response) {
+        if (error) {return "error getting results";}
+        var i;
+        console.log(q.query.search);
+        //console.dir(response);
+        var timeOfSearchRequest = new Date();
+        console.log(q.query.offset);
+        //inserts search term and time for search history storage
+        db.collection('searchHistory').insertOne({searchTerm: q.query.search,
+          timeRequested:timeOfSearchRequest.toDateString() + " " + timeOfSearchRequest.toLocaleTimeString() });
+
+        for (i = 0; i < 10; i++) {
+          var resultEntry = response.items[i];
+
+          if (resultEntry.pagemap.cse_thumbnail) {
+          resultArr.push({url:resultEntry.link, snippet:resultEntry.snippet,
+            thumbnail:resultEntry.pagemap.cse_thumbnail[0].src, context:resultEntry.pagemap.cse_image[0].src});
+          }
+        }
+        res.end(JSON.stringify(resultArr));
+      });
+
+    }
+    else {
+      fs.readFile('./static/index.html', function (err, data) {
+         if (err) {return err};
+
+         res.writeHead(200, {'Content-type': 'text/html'});
+         res.write(data);
+         res.end();
+      });
+    }
+})
+
+}).listen(3000);
